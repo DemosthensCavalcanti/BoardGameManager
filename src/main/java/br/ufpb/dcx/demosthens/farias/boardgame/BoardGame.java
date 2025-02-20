070e5517e05ab5d1b5c7422f7d1c38905b1a8ca0
DemosthensCavalcanti <demosthenscf@gmail.com>
2025-02-20 11:08:25 -0300

    Atualizando projeto, arrumando a casa.

--- src/main/java/br/ufpb/dcx/demosthens/farias/boardgame/BoardGame.java
+++ src/main/java/br/ufpb/dcx/demosthens/farias/boardgame/BoardGame.java
@@ -1,4 +1,52 @@
 package br.ufpb.dcx.demosthens.farias.boardgame;
 
-public class BoardGame {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class BoardGame implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private String category;
+    private int numberOfPlayers;
+
+    public BoardGame(String name, String category, int numberOfPlayers) {
+        this.name = name;
+        this.category = category;
+        this.numberOfPlayers = numberOfPlayers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        BoardGame boardGame = (BoardGame) obj;
+        return Objects.equals(name, boardGame.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "BoardGame{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", numberOfPlayers=" + numberOfPlayers +
+                '}';
+    }
 }
